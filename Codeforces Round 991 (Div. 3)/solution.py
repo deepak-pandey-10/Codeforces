ec4872fa5715f8ab288b724222f6e6f5689cca24
deepak-pandey-10 <dp9136332@gmail.com>
2024-12-16 02:46:45 +0530

    new question

---
+++ Codeforces Round 991 (Div. 3)/solution.py
@@ -0,0 +1,17 @@
+t=int(input())
+for i in range(t):
+    n,m=map(int,input().split())
+    ls=[]
+    for i in range(n):
+        s=input()
+        ls.append(s)
+    l=0
+    c=0
+    for i in ls:
+        l+=len(i)
+        if l<=m:
+            c+=1
+        else:
+            break
+    print(c)
+    
